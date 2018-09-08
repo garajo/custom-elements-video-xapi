7971964f4a87c871721cea6d08e187424a0423f6
garajo <garrickajo@gmail.com>
2018-09-07 23:36:24 -0700

    Advanced composition of components.

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const fs = require('fs')
 
-app.post('/answers/uuid-soetuh', function (req, res) {
+
+app.use('/answers/uuid-soetuh', bodyParser.json(),(req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Accept, Origin, Content-Type, access_token');
+  res.header('Access-Control-Allow-Credentials', 'true');
+
+  console.log('req', req)
   
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({
-    a: 1
-  }));
+  console.log(req.body)
+  console.log(JSON.stringify(req.body, null, 2))
+
+  fs.readFile('data/se98aoeu.json', (err, data) => {
+    if (err) throw err;
+    console.log(JSON.parse(data));
+  });
+
+  if (req.body) {
+    res.json({
+      a: 1,
+      b: 2,
+    });
+  }
 
 })
 
