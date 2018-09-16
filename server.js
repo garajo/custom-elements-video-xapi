bad6ce577df28c7396386c3592affbdb917c3772
garajo <garrickajo@gmail.com>
2018-09-16 00:00:48 -0700

    Beautified.

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const fs = require('fs')
-const { equallyGraded } = require('./lib/grading.js')
+const {
+  equallyGraded
+} = require('./lib/grading.js')
 
 const validateComponentId = (id) => id
 
@@ -20,9 +22,10 @@ app.use('/answers/:id', bodyParser.json(), (req, res, next) => {
       fs.readFile(`data/${req.params.id}.json`, (error, data) => {
         if (error) {
           console.log('error', error)
-          res.send({ error })
-        }
-        else if (req.body) {
+          res.send({
+            error
+          })
+        } else if (req.body) {
           res.json({
             answers: JSON.parse(data.toString()),
             grade: equallyGraded(JSON.parse(data), req.body),
@@ -36,7 +39,7 @@ app.use('/answers/:id', bodyParser.json(), (req, res, next) => {
   }
 })
 
-app.use('/saveanswers/:id', bodyParser.json(),(req, res, next) => {
+app.use('/saveanswers/:id', bodyParser.json(), (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'POST, OPTIONS')
   res.header('Access-Control-Allow-Headers', 'Accept, Content-Type, access_token')
@@ -50,7 +53,9 @@ app.use('/saveanswers/:id', bodyParser.json(),(req, res, next) => {
       fs.writeFile(`data/${validateComponentId(req.params.id)}.json`, JSON.stringify(req.body), (err) => {
         if (err) throw err
         console.log('The file has been saved!')
-        res.json({ "saved": req.body })
+        res.json({
+          "saved": req.body
+        })
       })
       break
     default:
